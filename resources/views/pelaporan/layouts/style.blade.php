b7148bc615f03767aebf44af5502bb8d5d3b5f11
nurfandi31 <fandinur241@gmail.com>
2026-07-29 10:11:18 +0700

    style

--- resources/views/pelaporan/layouts/style.blade.php
+++ resources/views/pelaporan/layouts/style.blade.php
@@ -1,84 +1,15 @@
 <style>
     * {
-        font-family: Arial, Helvetica, sans-serif;
+        font-family: 'Arial', sans-serif;
         box-sizing: border-box;
     }
 
-    html,
-    body {
-        margin: 0;
-        padding: 0;
-    }
-
-    body {
-        font-size: 12px;
-    }
-
     table {
-        width: 100%;
-        border-collapse: collapse;
         word-wrap: break-word;
-        page-break-inside: auto;
-        font-size: 11px;
-    }
-
-    thead {
-        display: table-header-group;
-    }
-
-    tfoot {
-        display: table-footer-group;
-    }
-
-    tr {
+        border-collapse: collapse;
         page-break-inside: avoid;
-        page-break-after: auto;
-    }
-
-    th,
-    td {
-        vertical-align: middle;
-    }
-
-    td:not(.p-0),
-    th:not(.p-0) {
-        padding: 2px 4px !important;
-        margin: 0 !important;
-    }
-
-    table td table tr td,
-    table td table tr th {
-        padding: 0 !important;
-    }
-
-    table.p tr td,
-    table.p tr th {
-        padding: 2px 4px !important;
-    }
-
-    table.p0 tr td,
-    table.p0 tr th {
-        padding: 0 !important;
-    }
-
-    p,
-    ul,
-    ol {
-        margin-top: 0;
-        margin-bottom: 6px;
-        line-height: 1.25;
-    }
-
-    ul,
-    ol {
-        padding-left: 20px;
-        page-break-inside: auto !important;
-    }
-
-    li {
-        margin-bottom: 2px;
-        line-height: 1.25;
-        text-align: justify;
+        width: 100%;
+        font-size: 11px;
     }
 
     table th {
@@ -86,8 +17,21 @@
         color: white;
     }
 
+    tr {
+        page-break-inside: avoid;
+        page-break-after: auto;
+    }
+
     tr.bold td {
         font-weight: bold;
+    }
+
+    td:not(.p-0),
+    th:not(.p-0) {
+        padding: 2px 4px !important;
+        margin: 0 !important;
+        border-collapse: collapse;
+        vertical-align: middle;
     }
 
     .row-white {
@@ -105,7 +49,7 @@
     }
 
     .break {
-        page-break-after: always;
+        page-break-after: always
     }
 
     .t {
